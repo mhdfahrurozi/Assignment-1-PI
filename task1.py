dfd6335518cd9e395cbdcb22e2a699fc4ce32eeb
mhdfahrurozi <107231096+mhdfahrurozi@users.noreply.github.com>
2024-03-02 14:35:30 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+#Task 1
+
+# Input data salary/Gaji
+gaji = float(input("Masukkan Jumlah Gaji: "))
+
+# Kalkulasi Gaji bulanan dengan dibagi 12 bulan
+gaji_perbulan = gaji / 12
+
+# print hasil gaji dengan pembulatan keatas bila desimal 
+print(round(gaji_perbulan))
+
+
+
+
