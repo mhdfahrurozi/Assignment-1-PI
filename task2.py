2163e3aa7c8007ef92e11cee49b186cd7bb0a49c
mhdfahrurozi <107231096+mhdfahrurozi@users.noreply.github.com>
2024-03-02 14:54:04 +0700

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+
+#task 2
+
+# input number dari user
+number = int(input("masukkan angka bilangan bulat: "))
+
+# Bagi bilangan 
+result = number / 3
+    
+#munculkan hasil dengan 3 angka desimal jika ada
+print("Hasil pembagian:", "{:.3f}".format(result))
+
+
+
+
+
