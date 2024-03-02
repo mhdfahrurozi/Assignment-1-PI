b96f67ef6684db7c6854c2c47c27993867f09e22
mhdfahrurozi <107231096+mhdfahrurozi@users.noreply.github.com>
2024-03-02 15:01:40 +0700

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,13 @@
+#Task 4
+
+# Input angka dari user
+number = int(input("Masukkan angka: "))
+
+# Jumlah kalkulasi dari angka 1 hingga angka yang dimasukkan user
+sum_number = sum(range(1, number + 1))
+
+# output dari penjumlahan
+print(sum_number)
+
+
+
